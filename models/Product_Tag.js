03c360020d2a3f6f762b2f0eeac8845e24123083
AndiS90 <87630680+AndiS90@users.noreply.github.com>
2021-11-03 04:27:32 -0500

    just realized this project has starter code

---
+++ models/Product_Tag.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create our Trip model
+class Product_Tag extends Model {}
+
+// create fields/columns for Product_Tag model
+Product_Tag.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+        unique: false
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id',
+        unique: false
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product_tag'
+  }
+);
+
+module.exports = Product_Tag;
---
+++ models/index.js
@@ -0,0 +1,38 @@
+const Category = require('./Category');
+const Product = require('./Product');
+const Tag = require('./Tag');
+const Product_Tag = require('./Product_Tag');
+
+
+
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
+
+
+// Define a category as having many products, thus creating a foreign key in the `product` table
+Category.hasMany(Product, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+});
+
+
+Product.belongsToMany(Tag, {
+  through: {
+    model: Product_Tag,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'Product_Tags'
+});
+
+Tag.belongsToMany(Product, {
+  through: {
+    model: Product_Tag,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'Tag_Products'
+});
+
+module.exports = { Category, Product, Tag, Product_Tag };
